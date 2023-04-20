51bfdbc204f4f14590438fbf1ba0d04241ac6f32
Elyahu Eaton <elyahu.eaton@siemens.com>
2023-04-20 11:59:48 +0300

    Init

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "ap-south-1" # Don't change the region
+}
+
+# Add your S3 backend configuration here
+terraform {
+  backend "s3" {
+    bucket = "3.devops.candidate.exam"
+    region  = "ap-south-1"
+    Key     = "Elyahu.Eaton"
+  }
+}
